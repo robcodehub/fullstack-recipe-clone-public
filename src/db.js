a46e1d66b8d96b490149ac5fb74dd56178d96764
Robert Wise <robcodehub@gmail.com>
2019-10-13 02:52:55 +0700

    created sync and seed with filler data

--- src/db.js
+++ src/db.js
@@ -76,9 +76,30 @@ Recipe.belongsTo(User);
 const syncAndSeed = async() => {
   await database.sync({ force: true })
 
-  const recipes = [{}]
+  const recipes = [{name: 'banana cake', cuisine: 'desert', healthScore: 3, directions: "add bananas, bake cake", imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT58qiVDeFITH1cSPewbes_epB-0aRufhq4tqJw8R3rJTKvNbuI"},
 
-  const users = [{}]
+  {name: 'chocolate cake', cuisine: 'desert', healthScore: 1, directions: "add chocolate, bake cake", imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT58qiVDeFITH1cSPewbes_epB-0aRufhq4tqJw8R3rJTKvNbuI"},
+
+  {name: 'pizza', cuisine: 'italian', healthScore: 5, directions: "add toppings, cook pizza", imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT58qiVDeFITH1cSPewbes_epB-0aRufhq4tqJw8R3rJTKvNbuI"},
+
+  {name: 'pasta', cuisine: 'italian', healthScore: 5, directions: "add pasta, cook pasta", imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT58qiVDeFITH1cSPewbes_epB-0aRufhq4tqJw8R3rJTKvNbuI"}
+]
+
+const [recipe1, recipe2, recipe3, recipe4] = await Promise.all(recipes.map(recipe => Receipe.create(receipe)));
+
+
+
+  const users = [
+    {username: "larry", email: "larry@larry.com", chefscore: 4, imageURL: "https://cdn1.iconfinder.com/data/icons/navigation-elements/512/user-login-man-human-body-mobile-person-512.png"},
+
+  {username: "curly", email: "curly@curly.com", chefscore: 7, imageURL: "https://cdn1.iconfinder.com/data/icons/navigation-elements/512/user-login-man-human-body-mobile-person-512.png"},
+
+  {username: "moe", email: "moe@moe.com", chefscore: 8, imageURL: "https://cdn1.iconfinder.com/data/icons/navigation-elements/512/user-login-man-human-body-mobile-person-512.png"},
+
+  {username: "sally", email: "sally@sallyy.com", chefscore: 7, imageURL: "https://cdn1.iconfinder.com/data/icons/navigation-elements/512/user-login-man-human-body-mobile-person-512.png"}
+]
+
+const [chef1, chef2, chef3, chef4 ] = await Promise.all(users.map(user => User.create(user)));
 }
 
 
