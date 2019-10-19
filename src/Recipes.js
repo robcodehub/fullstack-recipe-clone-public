dd0b54b2574fc90d20050efefbeeb011a8082993
Robert Wise <robcodehub@gmail.com>
2019-10-19 17:35:03 +0700

    button added on all recipes - no functionality yet

--- src/Recipes.js
+++ src/Recipes.js
@@ -11,7 +11,7 @@ const _Recipes = ({recipes}) => {
   console.log("RECIPES WITH CHEF =======", chefsTest)
   return (
     <div>
-      {recipes.map(recipe => <div key = {recipe.id} > <br /> Name: {recipe.name} <br /> Cuisine: {recipe.cuisine} <br /> Health Score: {recipe.healthScore} <br /> Ingredients: {recipe.ingredients} <br /> Directions: {recipe.directions} <br /> Image: <br /> <img height="250" width="250" src={recipe.imageURL} /> <Link to={`/recipes/${recipe.id}`}>Link to Recipe</Link> </div>)}
+      {recipes.map(recipe => <div key = {recipe.id} > <br />  <br /> Name: {recipe.name} <br /> Cuisine: {recipe.cuisine} <br /> Health Score: {recipe.healthScore} <br /> Ingredients: {recipe.ingredients} <br /> Directions: {recipe.directions} <br />  <br /> Image: <br /> <img height="250" width="250" src={recipe.imageURL} /> <br /> <Link to={`/recipes/${recipe.id}`}>Link to Recipe</Link> <br /> <br /> <button class="delete-recipe"> X </button> Delete Recipe Above <br /> </div>)}
       <br /> <br />
       <AddNewRecipeForm />
     </div>
