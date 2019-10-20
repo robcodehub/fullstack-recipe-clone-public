bfe0bbff91bbf97b6ed0208b0890174c18fe2cbe
Robert Wise <robcodehub@gmail.com>
2019-10-21 02:31:41 +0700

    removed cdns

--- src/Users.js
+++ src/Users.js
@@ -38,7 +38,7 @@ class _Users extends Component {
 
   return (
    <div>
-      {users.length} Users coming soon...
+      {users.length} in the database.
       {users.map(user => <div key={user.id}> <br />
         Chef Name: {user.username} <br />
         Chef Score: {user.chefScore} <br />
